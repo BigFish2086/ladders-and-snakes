4b02b8dbcf549e40b75d9c3fba2746478502f198
BigFish2086 <a8686.ibrahim@gmail.com>
2021-11-22 04:00:42 +0200

    add mytest.cpp

---
+++ src/mytest.cpp
@@ -0,0 +1,18 @@
+/*#include "Input.h"
+#include "Output.h"
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	CellPosition cp(-1,5);
+	cout << "old position\n";
+	cout << "H = " << cp.HCell() << " V = " <<cp.VCell();
+	cout << "\nNum " << cp.GetCellNum();
+	int move = 50;
+	cp.AddCellNum(move);
+	cout << "\nnew position\n";
+	cout << "H = " << cp.HCell() << " V = " << cp.VCell();
+	cout << "\nNum " << cp.GetCellNum();
+}
+*/
